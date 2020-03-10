a5e2a43f182a678d1945fe8517c979d9703d202d
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-03-10 13:30:39 +0100

    couchdb: Import Efraim’s suggestions

--- zpid/packages/couchdb.scm
+++ zpid/packages/couchdb.scm
@@ -29,17 +29,23 @@
      (origin
        (method url-fetch)
        (uri (string-append
-             "mirror://apache/couchdb/source/" version
-             "/apache-couchdb-" version ".tar.gz"))
+              "mirror://apache/couchdb/source/" version
+              "/apache-couchdb-" version ".tar.gz"))
        (sha256
         (base32
          "1nbz2vafzhp9jv8xna8cfnf99jwn22xs4ydzm426qx7yf0dbn2fi"))
        (modules '((guix build utils)))
-       ;; remove pre-built binary, will be rebuilt
+       ;; TODO: Unbundle fonts, others from share/docs/html/_static, share/www, share/server
+       ;; Remove pre-built binary, will be rebuilt.
        (snippet '(begin (delete-file "bin/rebar") #t))))
     (build-system gnu-build-system)
     (arguments
-     `(#:phases
+     `(#:make-flags '("release"
+                      "CC=gcc")
+       #:configure-flags (list "--skip-deps"
+                               "--spidermonkey-version"
+                               ,(version-major (package-version mozjs-60)))
+       #:phases
        (modify-phases %standard-phases
          (add-after 'unpack 'fix-paths
            (lambda* (#:key inputs #:allow-other-keys)
@@ -47,60 +53,50 @@
                (("/usr/include/mozjs-60")
                 (string-append (assoc-ref inputs "mozjs") "/include/mozjs-60")))
              (substitute* "Makefile"
-               ;; disable python-black code formatting
+               ;; Disable python-black code formatting.
                (("check: all python-black") "check: all")
-               ;; disable venv creation and package installation
+               ;; Disable venv creation and package installation.
                (("python3 -m venv .venv") "true")
                (("\\.venv/bin/python3 -m pip install -r requirements.txt") "true")
-               ;; do not test elixir, depends on `mix` and several external dependencies
+               ;; Do not test elixir, depends on `mix` and several external dependencies.
                (("@\\$\\(MAKE\\) elixir") "")
-               ;; use system python
-               (("\\.venv/bin/python3")
-                (string-append (assoc-ref inputs "python") "/bin/python3")))
-             #t))
-         (add-after 'unpack 'set-env
-           (lambda* (#:key inputs #:allow-other-keys)
-             ;; make sure rebar finds a C compiler
-             (setenv "CC" (string-append (assoc-ref inputs "gcc") "/bin/gcc"))
+               ;; Use system python.
+               (("\\.venv/bin/python3") (which "python3")))
              #t))
          (replace 'configure
-           (lambda* (#:key inputs #:allow-other-keys)
-             (invoke "./configure" "--skip-deps" "--spidermonkey-version" "60")
-             #t))
-         (replace 'build
-           (lambda _
-             ;; create a release build
-             (invoke "make" "release")
-             #t))
+           (lambda* (#:key (configure-flags '()) #:allow-other-keys)
+             (apply invoke "./configure" configure-flags)))
          (replace 'install
            (lambda* (#:key inputs outputs #:allow-other-keys)
-             ;; couchdb tries to bundle erts, use our own copy
-             (let ((erts-version "10.3.5.9"))
+             ;; CouchDB tries to bundle erts, use our own copy.
+             (let ((out          (assoc-ref outputs "out"))
+                   (erts-version "10.3.5.9"))
                (delete-file-recursively
-                (string-append "rel/couchdb/erts-" erts-version))
-               (substitute* '("rel/couchdb/bin/couchdb" "rel/couchdb/bin/remsh")
+                 (string-append "rel/couchdb/erts-" erts-version))
+               (substitute* '("rel/couchdb/bin/couchdb"
+                              "rel/couchdb/bin/remsh")
                  (("ROOTDIR=\\$\\{ERTS_BIN_DIR%/\\*\\}")
-                  (string-append "ROOTDIR=" (assoc-ref outputs "out")))
+                  (string-append "ROOTDIR=" out))
                  (("BINDIR=\"[^\"]+\"")
                   (string-append
-                   "BINDIR=" (assoc-ref inputs "erlang")
-                   "/lib/erlang/erts-" erts-version "/bin")))
-               (copy-recursively "rel/couchdb" (assoc-ref outputs "out")))
+                    "BINDIR=" (assoc-ref inputs "erlang")
+                    "/lib/erlang/erts-" erts-version "/bin")))
+               (copy-recursively "rel/couchdb" out))
              #t)))))
     (native-inputs
      ;; for tests
-     `(("python" ,python)
+     `(("curl" ,curl)
+       ("python" ,python)
+       ("python-hypothesis" ,python-hypothesis)
        ("python-nose" ,python-nose)
        ("python-requests" ,python-requests)
-       ("python-hypothesis" ,python-hypothesis)
-       ("curl" ,curl)
        ;; for documentation
        ("python-sphinx" ,python-sphinx)))
     (inputs
      `(("erlang" ,erlang)
        ("icu4c" ,icu4c)
-       ("openssl" ,openssl)
-       ("mozjs" ,mozjs-60)))
+       ("mozjs" ,mozjs-60)
+       ("openssl" ,openssl)))
     (home-page "https://couchdb.apache.org/")
     (synopsis "Document-oriented NoSQL database, implemented in Erlang")
     (description "CouchDB is a database that completely embraces the web.  Store
