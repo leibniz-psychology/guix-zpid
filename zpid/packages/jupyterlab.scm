9c3f8c2dfd6eff4c6bf9a5453dda9f450385d45c
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-03-20 08:51:59 +0100

    jupyterlab: Make jupyter-zpid actually installable
    Inherit python-notebook-zpid from python-notebook to resolve package conflicts
    and move python-requests-unixsocket above for clarity.

--- zpid/packages/jupyterlab.scm
+++ zpid/packages/jupyterlab.scm
@@ -14,54 +14,51 @@
   #:use-module (guix utils)
   #:use-module (guix build-system python))
 
+(define-public python-requests-unixsocket
+  (package
+    (name "python-requests-unixsocket")
+    (version "0.2.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "requests-unixsocket" version))
+       (sha256
+        (base32
+         "1sn12y4fw1qki5gxy9wg45gmdrxhrndwfndfjxhpiky3mwh1lp4y"))))
+    (build-system python-build-system)
+    (native-inputs
+     ;; pbr is required for setup only
+     `(("python-pbr" ,python-pbr)))
+    (propagated-inputs
+     `(("python-requests" ,python-requests)
+       ("python-urllib3" ,python-urllib3)))
+    (arguments
+     ;; tests depend on very specific package version, which are not available in guix
+     '(#:tests? #f))
+    (home-page
+     "https://github.com/msabramo/requests-unixsocket")
+    (synopsis
+     "Use requests to talk HTTP via a UNIX domain socket")
+    (description
+     "Use requests to talk HTTP via a UNIX domain socket")
+    (license license:asl2.0)))
+
 ;; custom version with socket patch
 (define-public python-notebook-zpid
   (package
+    (inherit python-notebook)
     (name "python-notebook-zpid")
-    (version "5.7.4")
     (source (origin
-              (method url-fetch)
-              (uri (pypi-uri "notebook" version))
-              (sha256
-               (base32
-                "0jm7324mbxljmn9hgapj66q7swyz5ai92blmr0jpcy0h80x6f26r"))
-              (patches (search-patches "jupyter-unix-domain-sockets-4835-5.7.4.patch"))))
-    (build-system python-build-system)
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (replace 'check
-           (lambda _
-             ;; These tests require a browser
-             (delete-file-recursively "notebook/tests/selenium")
-             ;; Some tests need HOME
-             (setenv "HOME" "/tmp")
-             ;; This file contains "warningfilters", which are not supported
-             ;; by this version of nose.
-             (delete-file "setup.cfg")
-             (with-directory-excursion "/tmp"
-               (invoke "nosetests" "-v"))
-             #t)))))
+              (inherit (package-source python-notebook))
+              (patches (append (origin-patches (package-source python-notebook))
+                               (search-patches "jupyter-unix-domain-sockets-4835-5.7.4.patch")))))
+    ;; required for unix socket patches’ tests (not run by python-notebook, but
+    ;; python-jupyterlab-server
     (propagated-inputs
-     `(("python-jupyter-core" ,python-jupyter-core)
-       ("python-nbformat" ,python-nbformat)
-       ("python-nbconvert" ,python-nbconvert)
-       ("python-prometheus-client" ,python-prometheus-client)
-       ("python-send2trash" ,python-send2trash)
-       ("python-terminado" ,python-terminado)
-       ("python-requests-unixsocket" ,python-requests-unixsocket)))
-    (native-inputs
-     `(("python-nose" ,python-nose)
-       ("python-sphinx" ,python-sphinx)
-       ("python-requests" ,python-requests)))
-    (home-page "http://jupyter.org/")
-    (synopsis "Web-based notebook environment for interactive computing")
-    (description
-     "The Jupyter HTML notebook is a web-based notebook environment for
-interactive computing.")
-    (properties `((python2-variant . ,(delay python2-notebook))))
-    (license license:bsd-3)))
+     `(("python-requests-unixsocket" ,python-requests-unixsocket)
+       ,@(package-propagated-inputs python-notebook)))))
 
+;; jupyter package, which uses our custom notebook
 (define jupyter-zpid-rewritten
   ((package-input-rewriting `((,python-notebook . ,python-notebook-zpid))) jupyter))
 
@@ -209,33 +206,4 @@ applications")
 	  "An extensible environment for interactive and reproducible computing,
 based on the Jupyter Notebook and Architecture.")
     (license license:bsd-3)))
-
-(define-public python-requests-unixsocket
-  (package
-    (name "python-requests-unixsocket")
-    (version "0.2.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "requests-unixsocket" version))
-       (sha256
-        (base32
-         "1sn12y4fw1qki5gxy9wg45gmdrxhrndwfndfjxhpiky3mwh1lp4y"))))
-    (build-system python-build-system)
-    (native-inputs
-     ;; pbr is required for setup only
-     `(("python-pbr" ,python-pbr)))
-    (propagated-inputs
-     `(("python-requests" ,python-requests)
-       ("python-urllib3" ,python-urllib3)))
-    (arguments
-     ;; tests depend on very specific package version, which are not available in guix
-     '(#:tests? #f))
-    (home-page
-     "https://github.com/msabramo/requests-unixsocket")
-    (synopsis
-     "Use requests to talk HTTP via a UNIX domain socket")
-    (description
-     "Use requests to talk HTTP via a UNIX domain socket")
-    (license license:asl2.0)))
 
