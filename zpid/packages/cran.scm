65c9f77fb1b8f237e634e2c7bd18120ec02aa30e
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-11-13 12:38:57 +0100

    cran: Fix a few linting issues

--- zpid/packages/cran.scm
+++ zpid/packages/cran.scm
@@ -255,13 +255,13 @@ Corrections")
      "Provides several cluster-robust variance estimators (i.e.,
 sandwich estimators) for ordinary and weighted least squares linear regression
 models, including the bias-reduced linearization estimator introduced by Bell
-and McCaffrey (2002) 
-<http://www.statcan.gc.ca/pub/12-001-x/2002002/article/9058-eng.pdf> and 
-developed further by Pustejovsky and Tipton (2017) 
-<DOI:10.1080/07350015.2016.1247004>. The package includes functions for estimating
+and McCaffrey (2002)
+<http://www.statcan.gc.ca/pub/12-001-x/2002002/article/9058-eng.pdf> and
+developed further by Pustejovsky and Tipton (2017)
+<DOI:10.1080/07350015.2016.1247004>.  The package includes functions for estimating
 the variance- covariance matrix and for testing single- and multiple-
-contrast hypotheses based on Wald test statistics. Tests of single regression
-coefficients use Satterthwaite or saddle-point corrections. Tests of multiple-
+contrast hypotheses based on Wald test statistics.  Tests of single regression
+coefficients use Satterthwaite or saddle-point corrections.  Tests of multiple-
 contrast hypotheses use an approximation to Hotelling's T-squared distribution.
 Methods are provided for a variety of fitted models, including lm() and mlm
 objects, glm(), ivreg (from package 'AER'), plm() (from package 'plm'), gls()
@@ -292,6 +292,35 @@ and lme() (from 'nlme'), robu() (from 'robumeta'), and rma.uni() and rma.mv()
     (synopsis
      "Meta-Analysis Methods Correcting for Publication Bias")
     (description
-     "This package provides meta-analysis methods that correct for publication bias and outcome reporting bias.  Four methods and a visual tool are currently included in the package.  The p-uniform method as described in van Assen, van Aert, and Wicherts (2015) <doi:10.1037/met0000025> can be used for estimating the average effect size, testing the null hypothesis of no effect, and testing for publication bias using only the statistically significant effect sizes of primary studies.  The second method in the package is the p-uniform* method as described in van Aert and van Assen (2019) <doi:10.31222/osf.io/zqjr9>.  This method is an extension of the p-uniform method that allows for estimation of the average effect size and the between-study variance in a meta-analysis, and uses both the statistically significant and nonsignificant effect sizes.  The third method in the package is the hybrid method as described in van Aert and van Assen (2017) <doi:10.3758/s13428-017-0967-6>.  The hybrid method is a meta-analysis method for combining an original study and replication and while taking into account statistical significance of the  original study.  The p-uniform and hybrid method are based on the statistical theory that the distribution of p-values is uniform conditional on the population effect size.  The fourth method in the package is the Snapshot Bayesian Hybrid Meta-Analysis Method as described in van Aert and van Assen (2018) <doi:10.1371/journal.pone.0175302>.  This method computes posterior probabilities for four true effect sizes (no, small, medium, and large) based on an original study and replication while taking into account publication bias in the original study.  The method can also be used for computing the required sample size of the replication akin to power analysis in null hypothesis significance testing.  The meta-plot is a visual tool for meta-analysis that provides information on the primary studies in the meta-analysis, the results of the meta-analysis, and characteristics of the research on the effect under study (van Assen and others, 2020).  Helper functions to apply the Correcting for Outcome Reporting Bias (CORB) method to correct for outcome reporting bias in a meta-analysis (van Aert & Wicherts, 2020).")
+     "This package provides meta-analysis methods that correct for publication
+bias and outcome reporting bias.  Four methods and a visual tool are currently
+included in the package.  The p-uniform method as described in van Assen, van
+Aert, and Wicherts (2015) <doi:10.1037/met0000025> can be used for estimating
+the average effect size, testing the null hypothesis of no effect, and testing
+for publication bias using only the statistically significant effect sizes of
+primary studies.  The second method in the package is the p-uniform* method as
+described in van Aert and van Assen (2019) <doi:10.31222/osf.io/zqjr9>.  This
+method is an extension of the p-uniform method that allows for estimation of
+the average effect size and the between-study variance in a meta-analysis, and
+uses both the statistically significant and nonsignificant effect sizes.  The
+third method in the package is the hybrid method as described in van Aert and
+van Assen (2017) <doi:10.3758/s13428-017-0967-6>.  The hybrid method is a
+meta-analysis method for combining an original study and replication and while
+taking into account statistical significance of the  original study.  The
+p-uniform and hybrid method are based on the statistical theory that the
+distribution of p-values is uniform conditional on the population effect size.
+The fourth method in the package is the Snapshot Bayesian Hybrid Meta-Analysis
+Method as described in van Aert and van Assen (2018)
+<doi:10.1371/journal.pone.0175302>.  This method computes posterior
+probabilities for four true effect sizes (no, small, medium, and large) based
+on an original study and replication while taking into account publication bias
+in the original study.  The method can also be used for computing the required
+sample size of the replication akin to power analysis in null hypothesis
+significance testing.  The meta-plot is a visual tool for meta-analysis that
+provides information on the primary studies in the meta-analysis, the results
+of the meta-analysis, and characteristics of the research on the effect under
+study (van Assen and others, 2020).  Helper functions to apply the Correcting
+for Outcome Reporting Bias (CORB) method to correct for outcome reporting bias
+in a meta-analysis (van Aert & Wicherts, 2020).")
     (license license:gpl2+)))
 
