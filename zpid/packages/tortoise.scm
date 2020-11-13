502cc5e1683345c3e679805380a48ba03c0446c1
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-11-13 12:39:16 +0100

    tortoise: Remove asynctest workaround
    Fixed upstream.

--- zpid/packages/tortoise.scm
+++ zpid/packages/tortoise.scm
@@ -10,13 +10,6 @@
   #:use-module ((gnu packages databases) #:prefix db:)
   #:use-module (gnu packages python-xyz))
 
-(define-public python-asynctest-notest
-  (package
-    (inherit python-asynctest)
-    (name "python-asynctest")
-    ;; tests fail with python 3.8
-    (arguments `(#:tests? #f))))
-
 (define-public python-pypika
   (package
     (inherit db:python-pypika)
@@ -34,8 +27,7 @@
 
 (define python-tortoise-orm-rewritten
   ((package-input-rewriting
-   `((,python-asynctest . ,python-asynctest-notest)
-     (,db:python-pypika . ,python-pypika)))
+   `((,db:python-pypika . ,python-pypika)))
     db:python-tortoise-orm))
 
 (define-public python-tortoise-orm
