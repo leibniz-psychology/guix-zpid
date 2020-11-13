8fc6ddc0b9bc37c5faebd95d3b4f9f76e313d7ec
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-11-13 12:38:27 +0100

    asyncssh: Upstreamed, remove

--- zpid/packages/asyncssh.scm
+++ zpid/packages/asyncssh.scm
@@ -1,60 +1,8 @@
 (define-module (zpid packages asyncssh)
-  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
-  #:use-module (guix download)
-  #:use-module (guix git-download)
-  #:use-module (guix utils)
-  #:use-module (guix build utils)
-  #:use-module (guix build-system python)
-  #:use-module (gnu packages)
-  #:use-module (gnu packages ssh)
-  #:use-module (gnu packages tls)
-  #:use-module (gnu packages python-crypto)
-  #:use-module (gnu packages python-xyz))
+  #:use-module ((gnu packages ssh) #:prefix guix:))
 
-;; python-gssapi with disabled tests, some of them fail now for unknown reasons
-(define-public python-gssapi-notests
-  (hidden-package (package/inherit python-gssapi
-    (name "python-gssapi")
-    (arguments `(#:tests? #f)))))
-
+; Upstreamed.
 (define-public python-asyncssh
-  (package
-    (name "python-asyncssh")
-    (version "2.3.0")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (pypi-uri "asyncssh" version))
-        (sha256
-          (base32
-            "0pi6npmsgx7l9r1qrfvg8mxx3i23ipff492xz4yhrw13f56a7ga4"))))
-    (build-system python-build-system)
-    (propagated-inputs
-     `(("python-cryptography" ,python-cryptography)
-       ("python-pyopenssl" ,python-pyopenssl)
-       ("python-gssapi" ,python-gssapi-notests)
-       ("python-bcrypt" ,python-bcrypt)))
-    ;; required for test suite
-    (native-inputs
-     `(("openssh" ,openssh)
-       ("openssl" ,openssl)))
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'disable-tests
-           (lambda* _
-            (substitute* "tests/test_agent.py"
-              ;; Test fails for unknown reason
-              (("(.+)async def test_confirm" all indent)
-                (string-append indent "@unittest.skip('disabled by guix')\n"
-                 indent "async def test_confirm"))))))))
-    (home-page "https://asyncssh.readthedocs.io/")
-    (synopsis
-      "Asynchronous SSHv2 client and server library")
-    (description
-      "AsyncSSH is a Python package which provides an asynchronous client and
-server implementation of the SSHv2 protocol on top of the Python 3.6+ asyncio
-framework")
-    (license license:epl2.0)))
+  (deprecated-package "python-asyncssh" guix:python-asyncssh))
 
