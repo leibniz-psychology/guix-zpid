f52f3d0cdf7e8173282cae4a0cb5b8b526e659e9
Lars-Dominik Braun <lars@6xq.net>
2022-09-09 10:49:54 +0200

    jupyter: Use search-patches

--- zpid/packages/jupyter.scm
+++ zpid/packages/jupyter.scm
@@ -1,10 +1,11 @@
 (define-module (zpid packages jupyter)
   #:use-module (guix packages)
+  #:use-module (gnu packages)
   #:use-module (gnu packages python-xyz)
   #:use-module (guix-science packages jupyter))
 
 (define-public python-nbformat-no-chmod
-  (package-with-extra-patches python-nbformat (list "python-nbformat-no-chmod.patch")))
+  (package-with-extra-patches python-nbformat (search-patches "python-nbformat-no-chmod.patch")))
 
 (define-public python-jupyterlab-for-notebook
   ((package-input-rewriting `((,python-nbformat . ,python-nbformat-no-chmod))) python-jupyterlab))
